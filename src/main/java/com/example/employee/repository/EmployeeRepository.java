15202cfad519318238b614794749e349e44ced6c
kikychn <kikychn@gmail.com>
2019-05-09 16:55:18 +0800

    找出一个薪资最高且公司ID是*的雇员以及该雇员的姓名

--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -18,6 +18,8 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     public Employee findByNameContainSpecialCharAndSalaryLargeThan(String name, Integer salary);
 
     //3.找出一个薪资最高且公司ID是*的雇员以及该雇员的姓名
+    @Query(value = "SELECT * FROM employee WHERE companyId = ?1 Order By salary DESC LIMIT 1", nativeQuery = true)
+    public Employee findByCompanyIdAndSalaryIsMax(Integer companyId);
 
     //4.实现对Employee的分页查询，每页两个数据
 
--- src/test/java/com/example/employee/EmployeeJPATest.java
+++ src/test/java/com/example/employee/EmployeeJPATest.java
@@ -61,7 +61,7 @@ public class EmployeeJPATest {
     public void should_return_employee_name_when_employee_salary_is_max_and_given_company_id_() throws Exception {
         //3.找出一个薪资最高且公司ID是1的雇员以及该雇员的name
         Employee expectedEmployee = new Employee(1,"xiaohong",19,"female",1,7000);
-        String actualName = null;
+        String actualName = employeeRepository.findByCompanyIdAndSalaryIsMax(1).getName();
         assertThat(actualName).isEqualTo(expectedEmployee.getName());
     }
 
