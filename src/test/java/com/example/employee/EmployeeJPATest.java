7989273684e8c05c17aa18e5dca6a0ec9e56e91e
kikychn <kikychn@gmail.com>
2019-05-09 19:17:09 +0800

    change the password to root for the root user

--- src/test/java/com/example/employee/EmployeeJPATest.java
+++ src/test/java/com/example/employee/EmployeeJPATest.java
@@ -32,7 +32,7 @@ public class EmployeeJPATest {
     public void setUp() throws Exception {
         //本地启动mysql，创建employee_db数据库
         Flyway flyway = new Flyway();
-        flyway.setDataSource("jdbc:mysql://localhost:3306/employee_db", "root", "Kikychina");
+        flyway.setDataSource("jdbc:mysql://localhost:3306/employee_db", "root", "root");
         flyway.clean();
         flyway.migrate();
     }
